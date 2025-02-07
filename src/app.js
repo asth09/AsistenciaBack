00e6f613b1daee328ffe015b99091e9bc026c35d
asth09 <asthackerman@gmail.com>
2025-02-07 14:48:01 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import authRoutes from './routes/auth.routes.js';
 import docenteRoutes from './routes/docente.routes.js';
@@ -27,6 +28,8 @@ const whitelist = [
   app.use(cors(corsOptions));
   app.use(express.json());
   app.use(cookieParser());
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use("/api", authRoutes);
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -43,6 +43,7 @@ export const login = async (req, res) => {
             secure: true,
             sameSite: 'None',
             path: '/',
+            partitioned: true,
             expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // expira en un dia si queres
         });
         return res.json({
@@ -50,6 +51,9 @@ export const login = async (req, res) => {
         })
     } catch (error) {
         console.log(error)
+        return res.json({
+            message: "error de login"
+        })
     }
 };
 
